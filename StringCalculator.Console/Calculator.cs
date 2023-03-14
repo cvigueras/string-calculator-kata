2e60dce85fdf401de5687e89d2ea51fa8dd8717b
cvigueras <carlosj.vigueras@gmail.com>
2023-03-14 11:32:11 +0100

    Refactor: Extract methods in production code.

--- StringCalculator.Console/Calculator.cs
+++ StringCalculator.Console/Calculator.cs
@@ -14,34 +14,44 @@ public class Calculator
 
     private static int SumNumbers(string numbers)
     {
-        var totalSum = 0;
-        if (numbers == "//;\n1;2")
+        if (numbers.Contains("//"))
         {
-            return 3;
+            numbers = TransformNumbersDelimiter(numbers);
         }
-        if (numbers == "//;\n6;12")
-        {
-            return 18;
-        }
-        if (numbers == "//;\n16;22")
-        {
-            return 38;
-        }
+
         if (numbers.Contains(","))
         {
             if (numbers.Contains("\n"))
             {
-                numbers = numbers.Replace("\n", ",");
+                numbers = TransformNumbersWithReturnLine(numbers);
             }
 
-            var arrayNumbers = numbers.Split(',');
-            foreach (var number in arrayNumbers)
-            {
-                totalSum += Convert.ToInt32(number);
-            }
-
-            return totalSum;
+            return SumTotalNumbers(numbers);
         }
         return Convert.ToInt32(numbers);
+    }
+
+    private static int SumTotalNumbers(string numbers)
+    {
+        var totalSum = 0;
+        var arrayNumbers = numbers.Split(',');
+        foreach (var number in arrayNumbers)
+        {
+            totalSum += Convert.ToInt32(number);
+        }
+
+        return totalSum;
+    }
+
+    private static string TransformNumbersWithReturnLine(string numbers)
+    {
+        return numbers.Replace("\n", ",");
+    }
+
+    private static string TransformNumbersDelimiter(string numbers)
+    {
+        var arrayNum = numbers.Split('\n');
+        var delimiter = arrayNum[0].Replace("//", string.Empty);
+        return arrayNum[1].Replace(delimiter, ",");
     }
 }
