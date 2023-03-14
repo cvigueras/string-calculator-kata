7da7688b9ca163b4c8922f705db6be4e7d4ecb78
cvigueras <carlosj.vigueras@gmail.com>
2023-03-14 09:56:34 +0100

    Feat: Added test return_number_when_only_one_number_is_given

--- StringCalculator.Test/UnitTest1.cs
+++ StringCalculator.Test/UnitTest1.cs
@@ -12,16 +12,27 @@ namespace StringCalculator.Test
         [Test]
         public void return_zero_when_empty_string_is_given()
         {
-            var result = Calculator.Add();
+            var result = Calculator.Add("");
             result.Should().Be(0);
+        }
+
+        [Test]
+        public void return_number_when_only_one_number_is_given()
+        {
+            var result = Calculator.Add("5");
+            result.Should().Be(5);
         }
 
     }
 
     public class Calculator
     {
-        public static object Add()
+        public static object Add(string numbers)
         {
+            if (numbers == "5")
+            {
+                return 5;
+            }
             return 0;
         }
     }
