58d92b8fc9c920643824bce1c121aae24ebeb7ab
cvigueras <carlosj.vigueras@gmail.com>
2023-03-31 15:04:58 +0200

    Refactor: Apply responsibility to CalculatorInput Class.

--- StringCalculator.Console/Calculator.cs
+++ StringCalculator.Console/Calculator.cs
@@ -5,18 +5,7 @@ public class Calculator
     public static int Add(CalculatorInput calculatorInput)
     {
         if (string.IsNullOrEmpty(calculatorInput.Numbers)) return 0;
-        var numbers = calculatorInput.Numbers;
-        var numbersCollection = new CalculatorInput(numbers).TransformNumbersDelimiter().ToList();
-        HasNegativeNumbers(numbersCollection);
-        return numbersCollection.Where(x => x <= 1000).ToArray().Sum();
-    }
-
-    public static void HasNegativeNumbers(IEnumerable<int> numbers)
-    {
-        var negativesNumbers = string.Join(",", numbers.Where(x => x < 0).ToArray());
-        if (negativesNumbers.Length > 0)
-        {
-            throw new InvalidDataException($"negatives not allowed {negativesNumbers}");
-        }
+        var calculator = new CalculatorInput(calculatorInput.Numbers);
+        return calculator.SumNumbers();
     }
 }
--- StringCalculator.Console/CalculatorInput.cs
+++ StringCalculator.Console/CalculatorInput.cs
@@ -1,18 +1,52 @@
 using System.Text.RegularExpressions;
-
 namespace StringCalculator.Console;
 
 public class CalculatorInput
 {
+    private IEnumerable<int> _numbersInt;
+    private readonly int _maxValue;
+    public string Numbers { get; }
+
     public CalculatorInput(string numbers)
     {
         Numbers = numbers;
+        _numbersInt = new List<int>();
+        _maxValue = 1000;
     }
-    public string Numbers { get; }
 
-    public IEnumerable<int> TransformNumbersDelimiter()
+    public int SumNumbers()
     {
-        var numbersCollection = new Regex(@"-?\d+(?!(?<=\[).+?(?=\]))").Matches(Numbers).ToArray();
-        return (numbersCollection.Aggregate(new int[] { }, (current, number) => current.Append(Convert.ToInt32(number.Value)).ToArray()));
+        _numbersInt = TransformNumbersDelimiter();
+        HasNegativeNumbers();
+        return _numbersInt.Where(x => x <= _maxValue).ToArray().Sum();
+    }
+
+    private IEnumerable<int> TransformNumbersDelimiter()
+    {
+        var matchNumbers = ExecuteRegex();
+        return GetNumbersMatch(matchNumbers);
+    }
+
+    private void HasNegativeNumbers()
+    {
+        if (GetNegativesNumbers().Length > 0)
+        {
+            throw new InvalidDataException($"negatives not allowed {GetNegativesNumbers()}");
+        }
+    }
+
+    private Match[] ExecuteRegex()
+    {
+        return new Regex(@"-?\d+(?!(?<=\[).+?(?=\]))").Matches(Numbers).ToArray();
+    }
+
+    private static IEnumerable<int> GetNumbersMatch(Match[] matchNumbers)
+    {
+        return matchNumbers.Aggregate(new int[] { }, (current, number) => current.Append(Convert.ToInt32(number.Value)).ToArray());
+    }
+
+    private string GetNegativesNumbers()
+    {
+        return string.Join(",", _numbersInt.Where(x => x < 0).ToArray());
     }
 }
